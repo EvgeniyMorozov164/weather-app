423acc295b3fc50736abc756b2acde160ed1a371
Evgeniy Morozov <131901919+EvgeniyMorozov164@users.noreply.github.com>
2024-09-21 20:32:42 +0400

    get weather api json response

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log("Hello from webpack");
+
+const input = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+
+const getForecast = async () => {
+  const API_KEY = "RGKYDRSS7BK7AE6DXH4NEADXJ";
+  const location = input.value;
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`;
+
+  const request = await fetch(url);
+  const data = await request.json();
+
+  console.log(data);
+}
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  getForecast();
+})
--- webpack.dev.js
+++ webpack.dev.js
@@ -7,28 +7,28 @@ module.exports = merge(common, {
   devServer: {
     static: './src',    
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/i,
-  //       use: ["style-loader", "css-loader"],
-  //     },
-  //     {
-  //       test: /\.(png|jpg|jpeg|svg|gif)$/i,
-  //       type: "asset/resource",
-  //     },
-  //     {
-  //       test: /\.(woff|woff2|eot|ttf|otf)$/i,
-  //       type: "asset/resource",
-  //     },
-  //     {
-  //       test: /\.(csv|tsv)$/i,
-  //       use: ["csv-loader"],
-  //     },
-  //     {
-  //       test: /\.xml$/i,
-  //       use: ["xml-loader"],
-  //     },
-  //   ],
-  // },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
+      },
+    ],
+  },
 });
